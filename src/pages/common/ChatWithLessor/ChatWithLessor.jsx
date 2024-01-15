d5619c735ce43e9cf96d388b2ab1fb4be7487005
Nguyễn Văn Vương <vuongnguyenvan282@gmail.com>
2024-01-15 12:00:59 +0700

    Update Chat

--- src/pages/common/ChatWithLessor/ChatWithLessor.jsx
+++ src/pages/common/ChatWithLessor/ChatWithLessor.jsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
 import HeaderNoSearch from '../../../components/component/HeaderNoSearch/HeaderNoSearch'
 import InboxCard from '../../../components/component/InboxCard/InboxCard'
 import './ChatWithLessor.scss'
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 
 function ChatWithLessor(){
+
+    const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState('');
+
+    const handleSendMessage = () => {
+        if (newMessage.trim() !== '') {
+        setMessages([...messages, { text: newMessage, sender: 'user' }]);
+        setNewMessage('');
+        }
+    };
+
     return (
         <div className="chatWithLessor">
             <HeaderNoSearch/>
@@ -25,7 +38,33 @@ function ChatWithLessor(){
                         <i>Thời gian phản hồi: 1 giờ</i>
                     </div>
                     <div className="bottomRightChat">
-                        
+                        <div className="chatBox">
+                            <div className="chatMessages">
+                                {messages.map((message, index) => (
+                                <div key={index} className={`message ${message.sender}`}>
+                                    {message.text}
+                                </div>
+                                ))}
+                            </div>
+                        </div>
+                        <div className="inputContainer">
+                            <div className='divSendMessage'>
+                                <input 
+                                    type="text"
+                                    placeholder='Nhập tin nhắn'
+                                    value={newMessage}
+                                    onChange={e => setNewMessage(e.target.value)}
+                                    className='inputMessage'
+                                    onKeyDown={(e) => {
+                                        if (e.key === 'Enter') {
+                                          e.preventDefault(); 
+                                          handleSendMessage();
+                                        }
+                                      }}
+                                />
+                                <button className='uploadMessage' style={{opacity: newMessage === "" ? 0 : 1}} onClick={() => handleSendMessage()}><span><ArrowUpwardIcon/></span></button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
