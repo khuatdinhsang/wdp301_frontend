0348aef8a47d06bbb17470a1f600f2fe687a9f2b
Vuong <vuongnguyenvan282@gmail.com>
2024-01-30 21:46:01 +0700

    Init Upload Bog

--- src/pages/lessor/UploadBlog/UploadBlog.jsx
+++ src/pages/lessor/UploadBlog/UploadBlog.jsx
@@ -1,10 +1,24 @@
 import axios from "axios";
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 import "./UploadBlog.scss"
 
 function UploadBlog(){
 
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [area, setArea] = useState();
+    const [money, setMoney] = useState();
+    const [addressDetail, setAddressDetail] = useState();
+    const [totalRoom, setTotalRoom] = useState();
+    const [rentalObject, setRentalObject] = useState();
+    const [expiredTime, setExpiredTime] = useState();
     const [hospitalImages, setHospitalImages] = useState([]);
+    const account = useSelector(state => state.account)
+    const [dataImg, setDataImg] = useState([])
+    const navigate = useNavigate();
+
 
 
     const getBase64Multiple = (file) => {
@@ -44,9 +58,13 @@ function UploadBlog(){
     });
     if (formImage) {
       await axios
-        .post(`api/upload/files`, formImage)
+        .post(`api/upload/files`, formImage,{
+            headers: {
+                    Authorization: `Bearer ${account?.token}`
+                }
+        })
         .then(async (res) => {
-          console.log("res", res.data);
+          setDataImg(res.data.data)
         })
         .catch((err) => {
           console.log(err);
@@ -56,65 +74,49 @@ function UploadBlog(){
     }
   };
 
-  const [hospitalImage, setHospitalImage] = useState({
-    file: "",
-    base64: "",
-  });
+  const handleOptionRental = (e) => {
+    setRentalObject(e.target.value);
+  }
 
-  const getBase64 = (file) => {
-    return new Promise((resolve, reject) => {
-      const fileReader = new FileReader();
-      fileReader.readAsDataURL(file);
-      fileReader.onload = () => {
-        resolve(fileReader.result);
-      };
-      fileReader.onerror = (error) => {
-        reject(error);
-      };
-    });
-  };
-  const convertImage = (e) => {
-    const file = e.target.files?.[0];
-    getBase64(file).then((res) => {
-      setHospitalImage({
-        file: file,
-        base64: res,
-      });
-    });
-  };
-  const submit = async (e) => {
-    e.preventDefault();
-    const formImage = new FormData();
-    formImage.append("file", hospitalImage.file);
-    if (hospitalImage.file.name) {
-      await axios
-        .post(`api/upload/file`, formImage)
-        .then(async (res) => {
-          console.log("res", res.data);
+  const handleUploadBlog = () => {
+    if(title === '' || description === '' || area === '' || money === '' || dataImg === [] || addressDetail === '' || totalRoom === '' || rentalObject === '' || expiredTime === ''){
+        console.log('Blank');
+    }else{
+        const blog = {
+            category: "rent",
+            title: title,
+            description: description,
+            area: area,
+            money: money,
+            image: dataImg,
+            video: [
+                "http://video1.jpg",
+                "http://video2.jpg"
+            ],
+            addressDetail: addressDetail,
+            totalRoom: totalRoom,
+            rentalObject: rentalObject,
+            expiredTime: expiredTime
+        }
+        
+        axios
+        .post('api/blog/create', blog, {
+            headers: {
+                    Authorization: `Bearer ${account?.token}`
+                }
         })
-        .catch((err) => {
-          console.log(err);
-        });
-    } else {
-      return console.log("Ảnh bệnh viện không được để trống", 1);
+        .then(res => {
+            console.log("Upload Success");
+            navigate('/')
+        })
+        .catch(err=> console.log(err))
     }
-  };
+  }
+ 
     return (
             <div className="uploadPage">
             <div className='uploadContain'>
                 <h3 className="uploadTitle">Upload Product</h3>
-                <form onSubmit={submit}>
-                    <input
-                    type="file"
-                    className="hospitalImage absolute bottom-0 right-0 w-8 h-8 rounded-[50%] cursor-pointer opacity-0"
-                    onChange={(e) => {
-                        convertImage(e);
-                    }}
-                    name="file"
-                    />
-                    <img src={hospitalImage.base64} alt="abc" width={100} height={100} />
-                    <button type="submit">Submit</button>
-                </form>
                 <div className="uploadContent">
                     <div className="inputBox">
                         <label htmlFor='inputName'>Tiêu đề Blog</label>
@@ -122,22 +124,28 @@ function UploadBlog(){
                             type="text"
                             className='inputName'
                             id='inputName'
-                            placeholder='Nhập tiêu đề'/>
+                            placeholder='Nhập tiêu đề'
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}/>
                     </div>
                     <div className="inputBox">
                         <label htmlFor='inputPrice'>Miêu tả</label>
                         <input
-                            type="number"
+                            type="text"
                             className='inputPrice'
                             id='inputPrice'
-                            placeholder='Nhập miêu tả'/>
+                            placeholder='Nhập miêu tả'
+                            value={description}
+                            onChange={e => setDescription(e.target.value)}/>
                     </div>
                     <div className="inputBox">
                         <label htmlFor='description'>Diện tích</label>
                         <textarea
                             className='inputPrice'
                             id='description'
-                            placeholder='Nhập diện tích'/>
+                            placeholder='Nhập diện tích'
+                            value={area}
+                            onChange={(e) => setArea(e.target.value)}/>
                     </div>
                     <div className="inputBox">
                         <label htmlFor='inputQuantity'>Giá</label>
@@ -145,34 +153,43 @@ function UploadBlog(){
                             type="number"
                             className='inputPrice'
                             id='inputQuantity'
-                            placeholder='Nhập giá tiền'/>
+                            placeholder='Nhập giá tiền'
+                            value={money}
+                            onChange={(e)=> setMoney(e.target.value)}/>
                     </div>
                     <div className="inputBox">
                          <form onSubmit={submitMultipleImg}>
-                            <label htmlFor="">Ảnh</label>
-                            <input
-                            type="file"
-                            multiple="true"
-                            className="hospitalImage absolute bottom-0 right-0 w-8 h-8 rounded-[50%] cursor-pointer opacity-0 inputPrice"
-                            onChange={(e) => {
-                                convertMultipleImage(e);
-                            }}
-                            name="files"
-                            />
-                            {hospitalImages.map((img) => {
-                            return <img src={img.base64} alt="abc" width={100} height={100} />;
-                            })}
-                            <button type="submit">Xác nhận Ảnh</button>
+                            <div className="inputBox">
+                                <label htmlFor="">Ảnh</label>
+                                <input
+                                type="file"
+                                multiple="true"
+                                className="hospitalImage absolute bottom-0 right-0 w-8 h-8 rounded-[50%] cursor-pointer opacity-0 inputPrice"
+                                onChange={(e) => {
+                                    convertMultipleImage(e);
+                                }}
+                                name="files"
+                                />
+                                <button type="submit" className="submitImgUpload">Xác nhận</button>
+                            </div>
                         </form>
                     </div>
+                    {hospitalImages ?  <div className="=listImg">
+                            {hospitalImages.map((img,index) => {
+                            return <img key={index} src={img.base64} alt="abc" width={100} height={100} />;
+                            })}                       
+                    </div>:<></>}
+                   
 
                     <div className="inputBox">
                         <label htmlFor='inputQuantity'>Địa chỉ</label>
                         <input
-                            type="number"
+                            type="text"
                             className='inputPrice'
                             id='inputQuantity'
-                            placeholder='Nhập địa chỉ trọ'/>
+                            placeholder='Nhập địa chỉ trọ'
+                            value={addressDetail}
+                            onChange={e => setAddressDetail(e.target.value)}/>
                     </div>
                     <div className="inputBox">
                         <label htmlFor='inputQuantity'>Tổng số phòng</label>
@@ -180,21 +197,64 @@ function UploadBlog(){
                             type="number"
                             className='inputPrice'
                             id='inputQuantity'
-                            placeholder='Nhập tổng số phòng'/>
+                            placeholder='Nhập tổng số phòng'
+                            value={totalRoom}
+                            onChange={e => setTotalRoom(e.target.value)}/>
                     </div>
                     <div className="inputBox">
                         <label htmlFor='inputQuantity'>Đối tượng thuê</label>
-                        <select >
-                            <option value="">Nam</option>
-                            <option value="">Nữ</option>
-                            <option value="">Cả nam và nữ</option>
-                        </select>
+                        <div className="inputRadio">
+                            <label htmlFor="both">
+                                <input 
+                                    type="radio" 
+                                    className='inputPrice'
+                                    name="rental"
+                                    id="both"
+                                    value={'both'}
+                                    checked={rentalObject === 'both'}
+                                    onChange={handleOptionRental}
+                                />
+                                Cả 2
+                            </label>
+                            <label htmlFor="male">
+                                <input 
+                                    type="radio" 
+                                    className='inputPrice'
+                                    id="male"
+                                    name="rental"
+                                    value={'male'}
+                                    checked={rentalObject === 'male'}
+                                    onChange={handleOptionRental}
+                                />
+                                Nam
+                            </label>
+                            <label htmlFor="female">
+                                 <input 
+                                    type="radio" 
+                                    className='inputPrice'
+                                    name="rental"
+                                    value={'female'}
+                                    id='female'
+                                    checked={rentalObject === 'female'}
+                                    onChange={handleOptionRental}
+                                />
+                                Nữ
+                            </label>
+                            
+                        </div>
                     </div>
                     <div className="inputBox">
-
+                        <label htmlFor='inputQuantity'>Ngày hết hạn bài đăng</label>
+                        <input
+                            type="date"
+                            className='inputPrice'
+                            id='inputQuantity'
+                            placeholder='Ngày hết hạn'
+                            value={expiredTime}
+                            onChange={e => setExpiredTime(e.target.value)}/>
                     </div>
                     <div className="submitForm">
-                        <button className='uploadBtn' >Upload</button>
+                        <span className="uploadBtn" onClick={() => handleUploadBlog()}>Đăng bài</span>
                     </div>
 
                 </div>
