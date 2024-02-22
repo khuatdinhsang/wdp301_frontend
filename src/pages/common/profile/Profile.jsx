82bee6a963844f7503b0804436370557b1a3327d
DucAnh <nguyenhuuducanh02@gmail.com>
2024-02-22 16:22:58 +0700

    Get profile

--- src/pages/common/profile/Profile.jsx
+++ src/pages/common/profile/Profile.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import './Profile.scss'
 import axios from "axios";
 import { useSelector } from 'react-redux';
@@ -6,9 +6,8 @@ import { useSelector } from 'react-redux';
 
 export default function Profile() {
   const [userDetail, setUserDetail] = useState()
-  const account = useSelector(state => state.account)
+  const account = useSelector(state => state.account);
   useEffect(() => {
-    console.log(account);
     if (!userDetail) {
       axios
         .get(`api/auth/profile`, {
@@ -18,44 +17,40 @@ export default function Profile() {
         })
         .then(res => {
           const userData = res.data.data
-          console.log(userData);
           setUserDetail(userData)
-          console.log(res.data.data)
         })
         .catch(error => {
           console.error(error)
         });
     }
   }, [userDetail, account])
-
+  console.log(userDetail);
   return (
-    <section className="profile" id="customers">
-      <div className="bottomDetailLessor">
-        <div className="leftDetailProfile">
-          <div class="card">
-            <img className='image' src="https://static2.yan.vn/YanNews/2167221/202102/facebook-cap-nhat-avatar-doi-voi-tai-khoan-khong-su-dung-anh-dai-dien-e4abd14d.jpg" alt="Avatar" />
-            <div className="container">
-              <h4><b>Đức Anh</b></h4>
-              <p>Renter</p>
-            </div>
-          </div>
-        </div>
-        <div className='blank'></div>
-        <div className='rightDetailProfile'>
-          <h2>About Đức Anh</h2>
-          <div className='blank'></div>
-          <span className="editBtn">
-            Edit Profile
-          </span>
-          <div className='blank'></div>
-          <div className='detailInfo'>
-            <i><b>Tên tài khoản: </b>0971508245</i><br></br>
-            <i><b>Gender: </b>male</i><br></br>
-            <i><b>Gmail: </b>ducanh02@gmail.com</i><br></br>
-            <i><b>Phone number: </b>0971508245</i><br></br>
+    <div className="bottomDetailLessor">
+      <div className="leftDetailProfile">
+        <div className="card">
+          <img className='image' src="https://static2.yan.vn/YanNews/2167221/202102/facebook-cap-nhat-avatar-doi-voi-tai-khoan-khong-su-dung-anh-dai-dien-e4abd14d.jpg" alt="Avatar" />
+          <div className="container">
+            <h4><b>{userDetail?.fullName}</b></h4>
+            <p>{userDetail?.role}</p>
           </div>
         </div>
       </div>
-    </section>
+      <div className='blank'></div>
+      <div className='rightDetailProfile'>
+        <h2>About {userDetail?.fullName}</h2>
+        <div className='blank'></div>
+        <span className="editBtn">
+          Edit Profile
+        </span>
+        <div className='blank'></div>
+        <div className='detailInfo'>
+          <i><b>Tên tài khoản: </b>{userDetail?.phone}</i><br></br>
+          <i><b>Gender: </b>male</i><br></br>
+          <i><b>Gmail: </b>abcd@gmail.com</i><br></br>
+          <i><b>Phone number: </b>{userDetail?.phone}</i><br></br>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -12,7 +12,8 @@ import Register from "../pages/common/register/Register"
 import WishList from "../pages/common/WishList/WishList"
 import BlogManager from "../pages/lessor/BlogManager/BlogManagement"
 import Profile from "../pages/common/profile/Profile"
-import HeaderNoSearch from "../components/component/HeaderNoSearch/HeaderNoSearch"import UploadBlog from "../pages/lessor/UploadBlog/UploadBlog"
+import HeaderNoSearch from "../components/component/HeaderNoSearch/HeaderNoSearch"
+import UploadBlog from "../pages/lessor/UploadBlog/UploadBlog"
 
 
 const publicRoutes = [
@@ -25,11 +26,10 @@ const publicRoutes = [
     { path: `/wishlist/:slug`, component: DetailWishList, layout: HeaderOnly },
     { path: '/inbox', component: ChatWithLessor, layout: null },
     { path: '/admin/dashboard', component: Dashboard, layout: null },
-    { path: '/admin/blogManager', component: BlogManagerAdmin, layout: null },
     { path: '/admin/editBlog/:slug', component: EditBlogManager, layout: null },
     { path: '/lessor/blogManager', component: BlogManager, layout: null },
     { path: '/lessor/uploadBlog', component: UploadBlog, layout: null },
-    { path: '/profile', component: Profile, layout: null },
+    { path: '/profile', component: Profile, layout: HeaderOnly },
 ]
 
 const adminRoutes = [
